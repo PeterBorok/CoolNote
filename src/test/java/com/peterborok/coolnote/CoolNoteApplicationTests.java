16903943b99f238f4b717d14ae4a57493d424b76
PeterBorok <peter.borok@gmail.com>
2021-11-12 10:02:49 +0100

    spring initialized

---
+++ src/test/java/com/peterborok/coolnote/CoolNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peterborok.coolnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
