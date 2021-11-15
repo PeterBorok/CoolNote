3010a7cdecc4f2faf7cd4548b101df4752daf653
PeterBorok <peter.borok@gmail.com>
2021-11-15 10:12:16 +0100

    routes added to controller

--- src/main/java/com/peterborok/coolnote/controller/NoteController.java
+++ src/main/java/com/peterborok/coolnote/controller/NoteController.java
@@ -1,7 +1,23 @@
 package com.peterborok.coolnote.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/")
 public class NoteController {
+
+    @GetMapping(value = "note")
+    public String displayAddNoteForm() {
+        return "note";
+    }
+
+    @PostMapping(value = "note")
+    public String processAddNoteForm(@RequestParam String newNote){
+        return "note";
+    }
+
 }
